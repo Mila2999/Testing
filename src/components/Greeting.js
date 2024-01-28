0c17b99cf13b6059f41a3f95f80b28d5d190b194
mila2999 <mila.savonik@gmail.com>
2024-01-28 12:35:04 +0200

    Testing Asynchronous Code

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import Output from './Output';
 const Greeting = () => {
   const [changedText, setChangedText] = useState();
   const changeTextHandler = () => {
@@ -9,8 +9,8 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello Sara!</h2>
-      {!changedText && <p>It's good to see you!</p>}
-      {changedText && <p>Changed!</p>}
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
